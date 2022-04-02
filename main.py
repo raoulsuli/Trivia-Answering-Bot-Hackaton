def3943a3a6da741fe98b93348fc2f3b2d5eaf34
Raoul <sulimoviciraoul@yahoo.com>
2022-04-02 12:47:26 +0300

    write to file

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
-import sys, requests
+import sys, requests, os
 from googlesearch import search
 from bs4 import BeautifulSoup
 
 query = sys.argv[1]
 
-for url in search(query, num = 1, stop = 1, pause = 2):
+idx = 0
+
+def write_to_file(idx, text):
+    if not os.path.exists("output"):
+        os.mkdir("output")
+    f = open(f"output/{idx}.txt", 'w')
+    f.write(result)
+    f.close()
+
+for url in search(query, num = 10, stop = 10, pause = 2):
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html5lib')
-    print(soup.prettify())
+    result = soup.get_text()
+
+    write_to_file(idx, result)
+
+    idx += 1
