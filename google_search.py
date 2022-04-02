3dd148e4081dcc3c733ad5fbdc7807f64495b6dc
Raoul <sulimoviciraoul@yahoo.com>
2022-04-02 17:50:28 +0300

    google search

--- google_search.py
+++ google_search.py
@@ -1,39 +1,42 @@
-import requests
+import requests, re
+from string import punctuation
 
-# get the API KEY here: https://developers.google.com/custom-search/v1/overview
-API_KEY = "AIzaSyBhCgIZg7Dws2uciAPnh2F8J7KyMTShi8Q"
-# get your Search Engine ID on your CSE control panel
-SEARCH_ENGINE_ID = "e504c2a0af6824094"
+def flatten(t):
+    return [item for sublist in t for item in sublist]
 
-EXACT_TERMS = "lead singer"
+def get_data():
 
-# the search query you want
-query = "Who is the lead singer of band Vita de Vie?"
-# using the first page
-page = 1
-# constructing the URL
-# doc: https://developers.google.com/custom-search/v1/using_rest
-# calculating start, (page=2) => (start=11), (page=3) => (start=21)
-start = (page - 1) * 10 + 1
-url = f"https://www.googleapis.com/customsearch/v1?exactTerms={EXACT_TERMS}&key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
+    # get the API KEY here: https://developers.google.com/custom-search/v1/overview
+    API_KEY = "AIzaSyBhCgIZg7Dws2uciAPnh2F8J7KyMTShi8Q"
+    # get your Search Engine ID on your CSE control panel
+    SEARCH_ENGINE_ID = "e504c2a0af6824094"
 
-# make the API request
-data = requests.get(url).json()
+    EXACT_TERMS = "lead singer"
 
-# get the result items
-search_items = data.get("items")
-# iterate over 10 results found
-for i, search_item in enumerate(search_items, start=1):
-    # get the page title
-    title = search_item.get("title")
-    # page snippet
-    snippet = search_item.get("snippet")
-    # alternatively, you can get the HTML snippet (bolded keywords)
-    html_snippet = search_item.get("htmlSnippet")
-    # extract the page url
-    link = search_item.get("link")
-    # print the results
-    print("="*10, f"Result #{i+start-1}", "="*10)
-    print("Title:", title)
-    print("Description:", snippet)
-    print("URL:", link, "\n")
+    # the search query you want
+    query = "Who is the lead singer of band Vita de Vie?"
+    # using the first page
+    page = 1
+    # constructing the URL
+    # doc: https://developers.google.com/custom-search/v1/using_rest
+    # calculating start, (page=2) => (start=11), (page=3) => (start=21)
+    start = (page - 1) * 10 + 1
+    url = f"https://www.googleapis.com/customsearch/v1?exactTerms={EXACT_TERMS}&key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
+
+    # make the API request
+    data = requests.get(url).json()
+
+    # get the result items
+    search_items = data.get("items")
+
+    # iterate over 10 results found
+    output = []
+    for i, search_item in enumerate(search_items, start=1):
+        title = search_item.get("title")
+        snippet = search_item.get("snippet")
+        text = f'{title} {snippet}'
+        chars = f'{re.escape(punctuation)}”'
+        output.append(re.sub(r'['+chars+']', '', text).split())
+    return " ".join(flatten(output))
+
+get_data()
