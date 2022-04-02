ac9003faec258082107397644b8a6b67866bb97b
Robert Caplan <robert.caplan@avira.com>
2022-04-02 13:56:05 +0300

    combine bigrams with filtered text

--- main.py
+++ main.py
@@ -13,9 +13,30 @@ answer_choices = []
 answer_type = ""
 idx = 0
 
+<<<<<<< Updated upstream
 NUM_PAGES = 10
 
 for url in search(query, num = NUM_PAGES, stop = NUM_PAGES, pause = 2):
+=======
+#lista bigrame care contin substantive
+words = nltk.word_tokenize(query)
+tagged_words = nltk.pos_tag(words)
+word_pairs = list(nltk.bigrams(tagged_words))
+nouns_bi = []
+for bigram in word_pairs:
+	if bigram[0][1].startswith('N') or bigram[1][1].startswith('N'):
+		nouns_bi.append(bigram)
+
+#soup = BeautifulSoup(r.content, 'html5lib')
+#print(nouns_bi)
+final_bigrams = []
+for item in nouns_bi:
+	final_bigrams.append(item[0][0] + ' ' + item[1][0])
+print(final_bigrams)
+#print(soup.prettify())
+
+for url in search(query, num = 1, stop = 1, pause = 2):
+>>>>>>> Stashed changes
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
 
@@ -34,3 +55,5 @@ for url in search(query, num = NUM_PAGES, stop = NUM_PAGES, pause = 2):
     f.close()
 
     idx += 1
+
+find(final_bigrams)
