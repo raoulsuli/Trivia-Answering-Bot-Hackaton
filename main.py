6c825f788d163c36b17c564d6cb7a1fa5d2cf085
Raoul <sulimoviciraoul@yahoo.com>
2022-04-02 14:15:35 +0300

    .

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from googlesearch import search
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+import shutil
 
 query = "Who is the lead singer of band Vita de vie?"
 
@@ -15,6 +16,9 @@ idx = 0
 
 NUM_PAGES = 10
 
+shutil.rmtree("output", ignore_errors=True)
+os.mkdir("output")
+
 #lista bigrame care contin substantive
 words = nltk.word_tokenize(query)
 tagged_words = nltk.pos_tag(words)
@@ -24,24 +28,20 @@ for bigram in word_pairs:
 	if bigram[0][1].startswith('N') or bigram[1][1].startswith('N'):
 		nouns_bi.append(bigram)
 
-#soup = BeautifulSoup(r.content, 'html5lib')
-#print(nouns_bi)
 final_bigrams = []
 for item in nouns_bi:
 	final_bigrams.append(item[0][0] + ' ' + item[1][0])
 print(final_bigrams)
-#print(soup.prettify())
 
 for url in search(query, num=NUM_PAGES, stop=NUM_PAGES, pause=2):
+    # if "ro." in url or ".ro" in url:
+    #     NUM_PAGES += 1
+    #     continue
+
     r = requests.get(url)
     soup = BeautifulSoup(r.content, 'html.parser')
 
     result = soup.find_all('p')
-    
-    if not os.path.exists("output"):
-        os.mkdir("output")
-
-    os.remove(f"output/{idx}.txt")
 
     f = open(f"output/{idx}.txt", 'a')
 
@@ -51,5 +51,3 @@ for url in search(query, num=NUM_PAGES, stop=NUM_PAGES, pause=2):
     f.close()
 
     idx += 1
-
-find(final_bigrams)
