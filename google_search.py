cabe1693ece50ce8863e4d91fb0414f6ad91a999
Raoul <sulimoviciraoul@yahoo.com>
2022-04-02 17:51:42 +0300

    google search

--- google_search.py
+++ google_search.py
@@ -4,24 +4,21 @@ from string import punctuation
 def flatten(t):
     return [item for sublist in t for item in sublist]
 
-def get_data():
+def get_data(query, terms):
 
     # get the API KEY here: https://developers.google.com/custom-search/v1/overview
     API_KEY = "AIzaSyBhCgIZg7Dws2uciAPnh2F8J7KyMTShi8Q"
     # get your Search Engine ID on your CSE control panel
     SEARCH_ENGINE_ID = "e504c2a0af6824094"
 
-    EXACT_TERMS = "lead singer"
-
-    # the search query you want
-    query = "Who is the lead singer of band Vita de Vie?"
     # using the first page
     page = 1
+    
     # constructing the URL
     # doc: https://developers.google.com/custom-search/v1/using_rest
     # calculating start, (page=2) => (start=11), (page=3) => (start=21)
     start = (page - 1) * 10 + 1
-    url = f"https://www.googleapis.com/customsearch/v1?exactTerms={EXACT_TERMS}&key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
+    url = f"https://www.googleapis.com/customsearch/v1?exactTerms={terms}&key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}"
 
     # make the API request
     data = requests.get(url).json()
